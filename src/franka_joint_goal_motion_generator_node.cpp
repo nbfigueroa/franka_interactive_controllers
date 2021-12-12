594f675d73d6f4588831b367d7f5fcca033accbd
Robot Queen <nbfigueroa@gmail.com>
2021-12-12 15:49:43 -0500

    Test some changes

--- src/franka_joint_goal_motion_generator_node.cpp
+++ src/franka_joint_goal_motion_generator_node.cpp
@@ -83,6 +83,8 @@ int main(int argc, char** argv) {
         std::cout << "Selected q_left_table_setting as goal" << std::endl;
         q_goal = {{-0.024844449233219813, 0.21341741475306059, 0.1671374870475969, -1.9734624159963505, 1.6724220574752517, 2.054275230565774, -0.36437520284810354}};
         break;           
+
+        
     }
   
     MotionGenerator motion_generator(0.6, q_goal);
