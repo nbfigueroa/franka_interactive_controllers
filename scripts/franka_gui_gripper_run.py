6a511473a7ccdac6a3a56e0a3ff860cc3babb15a
Figueroa Old Laptop <nbfigueroa@gmail.com>
2022-05-16 20:41:48 -0400

    Updates now working with latest robot system version >=4.2.1, libfranka version 0.9 and franka_ros >=0.8

--- scripts/franka_gui_gripper_run.py
+++ scripts/franka_gui_gripper_run.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 import shlex
-from psutil import Popen
 from tkinter import *
 from tkinter import messagebox
+from psutil import Popen
 
 root = Tk()
 root.title("Franka Gripper Control")
 root.geometry("300x75+1000+0")
 
-def open(): 
-   node_process = Popen(shlex.split('rosrun franka_interactive_controllers franka_gripper_run_node 1'))
-   messagebox.showinfo("Open Gripper", "Gripper Opened")
-   node_process.terminate()
+def open():
+	node_process = Popen(shlex.split('rosrun franka_interactive_controllers franka_gripper_run_node 1'))
+	messagebox.showinfo("Open Gripper", "Gripper Opened")
+	node_process.terminate()
 
-def close():   
-   node_process = Popen(shlex.split('rosrun franka_interactive_controllers franka_gripper_run_node 0'))
-   messagebox.showinfo("Close Gripper", "Gripper Closed")
-   node_process.terminate()
+def close():
+	node_process = Popen(shlex.split('rosrun franka_interactive_controllers franka_gripper_run_node 0'))
+	messagebox.showinfo("Close Gripper", "Gripper Closed")   
+	node_process.terminate()
 
 B1 = Button(root, text = "Open Gripper", command = open)
 B1.place(x = 30,y = 20)
--- scripts/libfranka_gui_gripper_run.py
+++ scripts/libfranka_gui_gripper_run.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 import shlex
-from psutil import Popen
 from tkinter import *
 from tkinter import messagebox
+from psutil import Popen
+
 
 top = Tk()
 top.title("Franka Gripper Control")
 top.geometry("300x75")
 
 def open(): 
-   node_process = Popen(shlex.split('rosrun franka_interactive_controllers libfranka_gripper_run 1'))
-   messagebox.showinfo("Open Gripper", "Gripper Opened")
-   node_process.terminate()
+	node_process = Popen(shlex.split('rosrun franka_interactive_controllers libfranka_gripper_run 1'))
+	messagebox.showinfo("Open Gripper", "Gripper Opened")
+	node_process.terminate()
 
 def close():   
-   node_process = Popen(shlex.split('rosrun franka_interactive_controllers libfranka_gripper_run 0'))
-   messagebox.showinfo("Close Gripper", "Gripper Closed")
-   node_process.terminate()
+	node_process = Popen(shlex.split('rosrun franka_interactive_controllers libfranka_gripper_run 0'))
+	messagebox.showinfo("Close Gripper", "Gripper Closed")
+	node_process.terminate()
 
 B1 = Button(top, text = "Open Gripper", command = open)
 B1.place(x = 30,y = 20)
